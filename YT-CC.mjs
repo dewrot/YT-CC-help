8b699bd1185c4fd0ea2a4aeed0af12f3f4f96646
dewrot <72311534+dewrot@users.noreply.github.com>
2022-08-02 11:55:55 -0400

    Update YT-CC.mjs

--- YT-CC.mjs
+++ YT-CC.mjs
@@ -1,5 +1,7 @@
-//const fs = require('fs');
-//const ytdl = require('ytdl-core');
+import fs from'fs'
+import ytdl from 'ytdl-core'
+import fetch from 'node-fetch'
+
 /*
 //cup of coffee
 const PORT = process.env.PORT || 3000;
@@ -10,34 +12,28 @@ setInterval(function() {
     http.get(`https://yt-cc.herokuapp.com/`);
 }, 300000);
 */
-/*
-ytdl('https://www.youtube.com/watch?v=Vlnl0v3jayU', { filter: 'audioonly'})
-  .pipe(fs.createWriteStream('video.mp3'));
-*/
 
-import fetch from 'node-fetch'
-import pkg from 'node-fetch'
-import pkg2 from 'node-fetch'
-import pkg3 from 'node-fetch'
-import pkg4 from 'node-fetch'
-import pkg5 from 'node-fetch'
-import pkg6 from 'node-fetch'
-
-
-const { Blob } = pkg;
-const { blobFrom } = pkg2;
-const { blobFromSync } = pkg3;
-const { File } = pkg4;
-const { fileFrom } = pkg5;
-const { fileFromSync } = pkg6;
-
-async function postFile(){
-  const mimetype = 'text/plain'
-  const blob = pkg6('test.txt', mimetype)
-  const url = 'https://tempfiles.ninja/api/upload'
-  const response = await fetch(url, { method: 'POST', body: blob })
-  const data = await response.json()
-  console.log(data.download_url)
+async function getVideo() {
+    ytdl('https://www.youtube.com/watch?v=yxu0qHbG_2c', { filter: 'audioonly'})
+      .pipe(fs.createWriteStream('video.mp3'))
 };
 
-postFile();
+async function postFile(){
+    //const mimetype = 'audio/mpeg'
+    const blob = await fs.promises.readFile('./video.mp3')
+    const url = 'https://tempfiles.ninja/api/upload'
+    const response = await fetch(url, { method: 'POST', body: blob, headers: { "Content-Type": mimetype } })
+    const data = await response.json()
+    
+    console.log(data.download_url)
+  };
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+async function runThings() {
+    getVideo()
+    await delay(20000);
+    console.log('Waited 20 seconds so file can download...')
+    postFile()
+};
+
+runThings();
